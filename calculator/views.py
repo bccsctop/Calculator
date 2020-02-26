b518822fa2b23d3ed330984baa5cb8599cf50afd
Siri Kijjaboriboon <s6101012620164@email.kmutnb.ac.th>
2020-02-26 18:04:30 +0700

    Can subtract mutiple divide

--- calculator/views.py
+++ calculator/views.py
@@ -3,14 +3,30 @@ from django.shortcuts import render , redirect
 
 
 def index(request):
+
     if request.method == 'POST' and request.POST.get('input1','') and request.POST.get('input2','')  :
-        x=request.POST.get('input1','')
-        y=request.POST.get('input2','')
+        #set variable
+        x = request.POST.get('input1','')
+        y = request.POST.get('input2','')
 
         #plus
         if request.POST.get('plus','') :
-            result= float(x)+float(y)
+            result = float(x) + float(y)
             return render(request, 'calculator/index.html',{'result': result})
 
+        #subtract
+        if request.POST.get('subtract',''):
+            result = float(x) - float(y)
+            return render(request, 'calculator/index.html',{'result': result})
 
+        #multiple
+        if request.POST.get('multiple',''):
+            result = float(x) * float(y)
+            return render(request, 'calculator/index.html',{'result': result})
+
+        #divide
+        if request.POST.get('divide',''):
+            result = float(x) / float(y)
+            return render(request, 'calculator/index.html',{'result': result})
+            
     return render(request,'calculator/index.html')
